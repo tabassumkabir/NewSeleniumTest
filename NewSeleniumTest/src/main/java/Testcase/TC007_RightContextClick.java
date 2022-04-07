e214736ac56e0ecf49fdad937eb58bb8cb3e243a
tabassumkabir <tabassumkabir123@gmail.com>
2022-04-07 14:28:35 +0600

    doubleclick

---
+++ NewSeleniumTest/src/main/java/Testcase/TC007_RightContextClick.java
@@ -0,0 +1,31 @@
+package Testcase;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import Base.DriverSetup;
+
+@Test
+public class TC007_RightContextClick extends DriverSetup{
+	 public static String Url="https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_select_multiple";
+	
+	
+	public static void doubleClickRightClick() throws InterruptedException {
+		driver.get(Url);
+		new WebDriverWait(driver,Duration.ofSeconds(5));
+		driver.manage().window().maximize();
+		driver.switchTo().frame("iframeResult");
+		Actions a =new Actions(driver);
+		WebElement ele=driver.findElement(By.xpath("//p[contains(text(),'The multiple attribute specifies that multiple options can be selected at once:')]"));
+		a.doubleClick(ele).build().perform();
+		
+		
+		
+	}
+
+}
