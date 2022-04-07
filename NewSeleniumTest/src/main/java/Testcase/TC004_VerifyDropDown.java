133a9a685d5278c32eaf1332649167abd2c211c9
tabassumkabir <tabassumkabir123@gmail.com>
2022-04-07 11:52:51 +0600

    Refresh page

--- NewSeleniumTest/src/main/java/Testcase/TC004_VerifyDropDown.java
+++ NewSeleniumTest/src/main/java/Testcase/TC004_VerifyDropDown.java
@@ -1,9 +1,12 @@
 package Testcase;
 
+import java.awt.Desktop.Action;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
@@ -24,6 +27,8 @@ public class TC004_VerifyDropDown extends DriverSetup{
 		driver.switchTo().frame("iframeResult");
 		WebElement e =driver.findElement(By.name("cars"));
 		Select s=new Select(e);
+		Actions a =new Actions(driver);
+		a.keyDown(Keys.CONTROL).click(s.getOptions().get(3)).click(s.getOptions().get(1)).keyUp(Keys.CONTROL).build().perform();
 		s.selectByIndex(1);
 		
 		
---
+++ NewSeleniumTest/src/main/java/Testcase/TC005_Refresh.java
@@ -0,0 +1,27 @@
+package Testcase;
+
+import java.time.Duration;
+
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import Base.DriverSetup;
+
+@Test
+public class TC005_Refresh extends DriverSetup{
+	
+	public static String Url="https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_select_multiple";
+	
+	
+	public static void refreshPage() {
+		
+		driver.get(Url);
+		new WebDriverWait(driver,Duration.ofSeconds(5));
+		driver.manage().window().maximize();
+		driver.navigate().to("https://www.google.com/");
+		driver.navigate().back();
+		driver.navigate().forward();
+		driver.navigate().refresh();
+	}
+
+}
