226e4530bff7a9487cbeabe0ac56589c5904220a
tabassumkabir <tabassumkabir123@gmail.com>
2022-04-07 12:41:31 +0600

    Scroll

---
+++ NewSeleniumTest/src/main/java/Testcase/TC006_Scroll.java
@@ -0,0 +1,34 @@
+package Testcase;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.devtools.idealized.Javascript;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import Base.DriverSetup;
+
+@Test
+public class TC006_Scroll extends DriverSetup{
+	
+	public static String Url="https://www.seleniumhq.org";
+	
+	public static void scrollCheck() throws InterruptedException {
+		
+		driver.get(Url);
+		new WebDriverWait(driver,Duration.ofSeconds(5));
+		driver.manage().window().maximize();
+		
+		JavascriptExecutor js=(JavascriptExecutor) driver;
+		js.executeScript("window.scrollTo(0,document.body.scrollHeight);");
+		Thread.sleep(5000);
+		js.executeScript("window.scrollTo(document.body.scrollHeight,0);");
+		Thread.sleep(5000);
+		
+		
+	}
+
+}
